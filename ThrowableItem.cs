663d6c39c685b06249593901320eac60e476cf64
Jacob C <jacob.j.carlson03@gmail.com>
2025-01-08 21:49:55 -0800

    made throwable_item register hits

--- ThrowableItem.cs
+++ ThrowableItem.cs
@@ -5,12 +5,15 @@ using System;
 public partial class ThrowableItem : Node3D
 {
 	[Export]
-	public float BreakVelocity = 15.0f;
+	public float BreakVelocity = 2.5f;
 	private RigidBody3D _rigidBody;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_rigidBody = GetNode<RigidBody3D>("XRToolsPickable");
+		GD.Print("Hello, player!");
+
+		_rigidBody.BodyEntered += _on_body_entered;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,14 +21,16 @@ public partial class ThrowableItem : Node3D
 	{
 
 	}
-	private void _on_body_entered(Node body)
+	
+	public void _on_body_entered(Node body)
 	{
-		if (body is PhysicsBody3D)
-		{
+		GD.Print($"HIT a {body.GetType()}!!!! Velocity: {_rigidBody.LinearVelocity.Length()}");
+		// if (body is PhysicsBody3D)
+		// {
 			if (_rigidBody.LinearVelocity.Length() >= BreakVelocity)
 			{
 				GD.Print("sugma my my balls");
 			}
-		}
+		// }
 	}
 }
