346b9a402b271ee16f3f1ade3a570d5d15cdf771
Jacob C <jacob.j.carlson03@gmail.com>
2025-01-13 22:25:40 -0800

    DIfferent tank steering controls. box around main scene to prevent tank from falling off

--- tank/Tank.cs
+++ tank/Tank.cs
@@ -27,12 +27,12 @@ public partial class Tank : VehicleBody3D
 
     public override void _PhysicsProcess(double delta)
     {
-		// const float steeringRange = (float)Math.PI / 2.0f;
-		// const float engineScale = 100.0f;
-		// Steering = (leverL.Value - leverR.Value) * steeringRange;
-		// EngineForce = (leverL.Value + leverR.Value) * -0.5f * engineScale;
-		Steering = leverL.Value * 1.5f;
-		EngineForce = leverR.Value * 90.0f;
+		const float steeringRange = (float)Math.PI / 3.0f;
+		const float engineScale = 100.0f;
+		Steering = (leverL.Value - leverR.Value) * steeringRange;
+		EngineForce = Math.Max(leverL.Value, leverR.Value) * 0.5f * engineScale;
+		// Steering = leverL.Value * 1.5f;
+		// EngineForce = leverR.Value * 90.0f;
         base._PhysicsProcess(delta);
     }
 }
