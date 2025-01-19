0a2c98621470edc6322a0bacd3e3c8301d668c74
Jacob C <jacob.j.carlson03@gmail.com>
2025-01-18 18:16:17 -0800

    Body of syringe now can be pushed down and up and stays with tank just fine

--- tank/SyringeBody.cs
+++ tank/SyringeBody.cs
@@ -5,21 +5,40 @@ public partial class SyringeBody : Area3D
 {
 	public bool press_to_hold = true;
 	bool _isPickedUp = false;
-
+	Vector3 _startGrabPos;
+	float _startSlidePos;
+	Node3D _grabber;
+	Label3D debugLabel;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		debugLabel = GetNode<Label3D>("Label3D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (_isPickedUp)
+		{
+			var currentGrabPos = _grabber.GlobalPosition;
+			var grabDiff = currentGrabPos - _startGrabPos;
+			Position = new Vector3(Position.X, _startSlidePos + grabDiff.Y, Position.Z);
+			debugLabel.Text = $"{_grabber.Name}: {currentGrabPos}\n{grabDiff.Y}";
+		}
+		else
+		{
+			debugLabel.Text = "N/A";
+		}
 	}
 
 	public void pick_up(Node3D by)
 	{
-		GD.Print("]]] Hello world");
+		_isPickedUp = true;
+		GD.Print($"{Name} GRABBED BY {by.Name}.");
+		_startGrabPos = by.GlobalPosition;
+		_startSlidePos = Position.Y;
+		_grabber = by;
 	}
 
 	public void let_go(Node3D by,
@@ -27,6 +46,7 @@ public partial class SyringeBody : Area3D
 		Vector3 p_angular_velocity)
 	{
 		_isPickedUp = false;
+		GD.Print($"{Name} RELEASED BY {by.Name}.");
 	}
 
 	public bool can_pick_up(Node3D by)
